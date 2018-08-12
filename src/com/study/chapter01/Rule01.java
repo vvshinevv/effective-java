738de1eea0ddf9ff857e92363b7de6fdfef9efda
최홍희 <vvshinevv@tmon.co.kr>
2018-08-12 18:33:27 +0900

    inital commit

---
+++ src/com/study/chapter01/Rule01.java
@@ -0,0 +1,12 @@
+package com.study.chapter01;
+
+/**
+ * Created by vvshinevv on 2018. 8. 12..
+ */
+public class Rule01 {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
